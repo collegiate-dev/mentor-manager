75914f08aa6b65ae269ce8efb90d6564b18ad9db
Ishaan Bansal <35862574+bshaan77@users.noreply.github.com>
2024-06-27 21:32:05 -0400

    add auth and home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,31 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Matches() {
   const matches = await db.query.matches.findMany();
+  return (
+    <div className="flex flex-wrap gap-4">
+      {matches.map((match) => (
+        <div key={match.studentId}>{match.studentId}</div>
+      ))}
+    </div>
+  );
+}
 
-  console.log("matches", matches);
+export default async function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {matches.map((match) => (
-          <div key={match.studentId}>{match.studentId}</div>
-        ))}
-      </div>
+      <SignedIn>
+        <Matches />
+      </SignedIn>
+      <SignedOut>
+        <div className="text-center">
+          <h1 className="text-4xl font-bold">Welcome to Mentor Manager</h1>
+          <p className="text-lg">Please sign in to continue</p>
+        </div>
+      </SignedOut>
     </main>
   );
 }

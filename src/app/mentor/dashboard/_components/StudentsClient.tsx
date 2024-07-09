b359fd3dc7a86520e722e869b95f8c815e7ae3af
Ishaan Bansal <35862574+bshaan77@users.noreply.github.com>
2024-07-09 16:38:42 -0400

    change file structure to create a mentor route

--- src/app/mentor/dashboard/_components/StudentsClient.tsx
+++ src/app/mentor/dashboard/_components/StudentsClient.tsx
@@ -32,7 +32,7 @@ const StudentsClient = ({ matches }: StudentsClientProps) => {
   }
 
   const handleButtonClick = (matchId: number) => {
-    router.push(`/matches/${matchId}`);
+    router.push(`/completeMeeting/${matchId}`);
   };
 
   return (

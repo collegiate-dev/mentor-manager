47cb4d4b5ee495cbf886b9746a7f90af4ae39f60
Ishaan Bansal <35862574+bshaan77@users.noreply.github.com>
2024-07-12 12:54:49 -0400

    recipients route

---
+++ src/app/api/mercury/recipients/route.ts
@@ -0,0 +1,17 @@
+import fetch from "node-fetch";
+
+const url = "https://backend.mercury.com/api/v1/accounts";
+
+const options = {
+  method: "GET",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${process.env.MERCURY_SECRET_TOKEN}`,
+  },
+};
+
+fetch(url, options)
+  .then((res) => res.json())
+  .then((json) => console.log(json))
+  .catch((err) => console.error("error:" + err));

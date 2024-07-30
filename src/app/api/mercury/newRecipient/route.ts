1d31cbd2d5679d693f7570844e15044cb5ce6d5d
Ishaan Bansal <35862574+bshaan77@users.noreply.github.com>
2024-07-29 22:18:34 -0700

    commenting out for now

--- src/app/api/mercury/newRecipient/route.ts
+++ src/app/api/mercury/newRecipient/route.ts
@@ -1,43 +1,43 @@
-import fetch from "node-fetch";
-import { type MentorDetails, type MercuryResponse } from "~/api/queries";
+// import fetch from "node-fetch";
+// import { type MentorDetails, type MercuryResponse } from "~/api/queries";
 
-export async function addRecipientToMercury(
-  mentorDetails: MentorDetails,
-): Promise<MercuryResponse> {
-  const url = "https://backend.mercury.com/api/v1/recipients";
-  const apiToken = process.env.MERCURY_SECRET_TOKEN;
+// export async function addRecipientToMercury(
+//   mentorDetails: MentorDetails,
+// ): Promise<MercuryResponse> {
+//   const url = "https://backend.mercury.com/api/v1/recipients";
+//   const apiToken = process.env.MERCURY_SECRET_TOKEN;
 
-  const payload = {
-    emails: [mentorDetails.email],
-    name: mentorDetails.fullname,
-    paymentMethod: mentorDetails.paymentMethod,
-    electronicRoutingInfo: {
-      address: mentorDetails.electronicRoutingInfo,
-      electronicAccountType: mentorDetails.electronicAccountType,
-      routingNumber: mentorDetails.routingNumber?.toString() ?? "", // Ensure it's a string
-      accountNumber: mentorDetails.accountNumber,
-    },
-  };
+//   const payload = {
+//     emails: [mentorDetails.email],
+//     name: mentorDetails.fullname,
+//     paymentMethod: mentorDetails.paymentMethod,
+//     electronicRoutingInfo: {
+//       address: mentorDetails.electronicRoutingInfo,
+//       electronicAccountType: mentorDetails.electronicAccountType,
+//       routingNumber: mentorDetails.routingNumber?.toString() ?? "", // Ensure it's a string
+//       accountNumber: mentorDetails.accountNumber,
+//     },
+//   };
 
-  console.log("Payload:", payload); // Log the payload
+//   console.log("Payload:", payload); // Log the payload
 
-  const options = {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${apiToken}`,
-    },
-    body: JSON.stringify(payload),
-  };
+//   const options = {
+//     method: "POST",
+//     headers: {
+//       Accept: "application/json",
+//       "Content-Type": "application/json",
+//       Authorization: `Bearer ${apiToken}`,
+//     },
+//     body: JSON.stringify(payload),
+//   };
 
-  try {
-    const response = await fetch(url, options);
-    const json = await response.json();
-    // console.log("Mercury Response:", json);
-    return json as MercuryResponse;
-  } catch (err) {
-    console.error("Error adding recipient to Mercury:", err);
-    throw new Error("Error adding recipient to Mercury");
-  }
-}
+//   try {
+//     const response = await fetch(url, options);
+//     const json = await response.json();
+//     // console.log("Mercury Response:", json);
+//     return json as MercuryResponse;
+//   } catch (err) {
+//     console.error("Error adding recipient to Mercury:", err);
+//     throw new Error("Error adding recipient to Mercury");
+//   }
+// }

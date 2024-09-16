ff7f43c01cf8471f82d6e21fab91b28775d5e47e
Ishaan Bansal <35862574+bshaan77@users.noreply.github.com>
2024-09-15 20:01:05 -0700

    plaidClient init

---
+++ src/lib/plaidClient.ts
@@ -0,0 +1,15 @@
+// /lib/plaidClient.ts
+import { Configuration, PlaidApi, PlaidEnvironments } from "plaid";
+
+const configuration = new Configuration({
+  basePath: PlaidEnvironments.sandbox, // or development/production
+  baseOptions: {
+    headers: {
+      "PLAID-CLIENT-ID": process.env.PLAID_CLIENT_ID!,
+      "PLAID-SECRET": process.env.PLAID_SECRET!,
+    },
+  },
+});
+
+const plaidClient = new PlaidApi(configuration);
+export default plaidClient;
